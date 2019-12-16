68a8872a7d9ca680a44ea14c29d82cdf3b76cf4e
Ronny Wieckardt <ronny@ckdt.nl>
2019-12-16 15:20:03 +0100

    editje hoor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,129 @@
 import {CountUp} from './countup.min.js';
 
+new fullpage('#fullpage', {
+  licenseKey: 'D62A0CA5-A6F748B1-AADF056F-D8133285',
+  navigation: true,
+  sectionsColor: [
+    '#29313E',
+    '#DAE1D9',
+    '#010fff',
+    '#506171',
+    '#FAF3ED',
+    '#29313E',
+    '#DAE1D9',
+    '#ff00ff'
+  ],
+  anchors: [
+    'intro',
+    'summary',
+    'industries',
+    'locations',
+    'projects',
+    'testimonials',
+    'time',
+    'tmp'
+  ],
+  afterRender: function() {
+    animateElement('.js-animate-start', 'bounce');
+  },
+  onLeave: function(origin, section, direction) {
+    console.log('from', origin.index, 'to', section.index);
+
+    /* slide 2 */
+    if (origin.index == 0 && section.index == 1) {
+      console.log('Slide 2');
+      countProjects();
+    }
+    /* slide 3 */
+    if (origin.index == 1 && section.index == 2) {
+      console.log('Slide 3');
+
+      document.querySelectorAll('.js-animate-li').forEach(el => {
+        animateElement(el, 'fadeInLeft');
+        console.log(`Element ${el.tagName} with ID #${el.id} says: ${el.textContent}`);
+      });
+    }
+    /* slide 4 */
+    if (origin.index == 2 && section.index == 3) {
+      console.log('Slide 4');
+    }
+    /* slide 5 */
+    if (origin.index == 3 && section.index == 4) {
+      console.log('Slide 5');
+    }
+    /* slide 6 */
+    if (origin.index == 4 && section.index == 5) {
+      console.log('Slide 6');
+    }
+    /* slide 7 */
+    if (origin.index == 5 && section.index == 6) {
+      console.log('Slide 7');
+    }
+    /* slide 8 */
+    if (origin.index == 6 && section.index == 7) {
+      console.log('Slide 8');
+    }
+    /* slide 9 */
+    if (origin.index == 7 && section.index == 8) {
+      console.log('Slide 9');
+    }
+    /* slide 10 */
+    if (origin.index == 8 && section.index == 9) {
+      console.log('Slide 10');
+    }
+  }
+});
+
+function animateElement(element, method, callback) {
+  console.log('animate', element, 'method', method, 'when done do:', callback);
+  var node = element;
+  if (typeof element === 'string') {
+    node = document.querySelector(element);
+  }
+  node.classList.add('animated', method);
+  function handleAnimationEnd() {
+    node.classList.remove('animated', method);
+    node.removeEventListener('animationed', handleAnimationEnd);
+
+    if (typeof callback === 'function') callback();
+  }
+  node.addEventListener('animationend', handleAnimationEnd);
+}
+
+function countProjects() {
+  var countID = setInterval(
+    function(e) {
+      const counter = document.querySelector('.js-animate-counter');
+      const options = {
+        startVal: 0,
+        duration: 5
+      };
+      let display = new CountUp(counter, 28, options);
+      if (!display.error) {
+        display.start();
+      } else {
+        console.error(display.error);
+      }
+      clearInterval(countID);
+    },
+    1000,
+    this
+  );
+}
+
+/* 
+COLORS
+#29313E 
+
+#DAE1D9;
+#AFC6B6;
+#FAF3ED;
+#29313E;
+#A17035;
+#EEDAD3;
+#506171;
+
+
 // set animate vars
 function animateCSS(element, animationName, callback) {
   console.log(typeof element);
@@ -21,30 +145,11 @@ function animateCSS(element, animationName, callback) {
   node.addEventListener('animationend', handleAnimationEnd);
 }
 
-/* addClass */
-function addClass(element, className) {
-  const node = document.querySelector(element);
-  if (!node.classList.contains(className)) {
-    node.classList.add(className);
-    return true;
-  }
-  return false;
-}
-
-/* removeClass */
-function removeClass(element, className) {
-  const node = document.querySelector(element);
-  if (node.classList.contains(className)) {
-    if (typeof className === 'string') node.classList.remove(className);
-    return true;
-  }
-  return false;
-}
-
+/*
 // init fullpage
 new fullpage('#fullpage', {
-  licenseKey: 'D62A0CA5-A6F748B1-AADF056F-D8133285',
-  navigation: true,
+ 
+  
   sectionsColor: ['#29313E', '#DAE1D9', '#010fff', '#506171', '#FAF3ED', '#29313E', '#DAE1D9', '#ff00ff'],
   anchors: ['intro', 'summary', 'industries', 'locations', 'projects', 'testimonials', 'time', 'superhi'],
   normalScrollElements: '.grid--cards',
@@ -99,3 +204,4 @@ new fullpage('#fullpage', {
     }
   }
 });
+*/
