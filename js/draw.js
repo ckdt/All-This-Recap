f5ce9c7f06f87f5a6b679bb9a01346fec5e9977d
Ronny Wieckardt <ronny@ckdt.nl>
2019-12-22 09:28:04 +0100

    Added delay in clear canvas

--- js/draw.js
+++ js/draw.js
@@ -1,58 +1,80 @@
-const canvas = document.querySelector('#drawingboard');
+var aniFrame;
 
-canvas.width = window.innerWidth * 2;
-canvas.height = window.innerHeight * 2;
+function initCanvas() {
+  const canvas = document.querySelector('#drawingboard');
 
-canvas.style.width = window.innerWidth + 'px';
-canvas.style.height = window.innerHeight + 'px';
+  canvas.width = window.innerWidth * 2;
+  canvas.height = window.innerHeight * 2;
 
-const context = canvas.getContext('2d');
-context.scale(2, 2);
+  canvas.style.width = window.innerWidth + 'px';
+  canvas.style.height = window.innerHeight + 'px';
 
-let aimX = null;
-let aimY = null;
-let currentX = null;
-let currentY = null;
+  const context = canvas.getContext('2d');
+  context.scale(2, 2);
 
-let i = 0;
-const images = ['img/at-logo.png', 'img/at-2019.png', 'img/at-more.png'].map(src => {
-  const image = document.createElement('img');
-  image.src = src;
-  return image;
-});
+  let aimX = null;
+  let aimY = null;
+  let currentX = null;
+  let currentY = null;
 
-document.addEventListener('mousemove', function(e) {
-  aimX = e.pageX;
-  aimY = e.pageY;
-  if (currentX === null) {
-    currentX = e.pageX;
-    currentY = e.pageY;
-  }
-});
+  let i = 0;
+  const images = ['img/at-logo.png', 'img/at-2019.png', 'img/at-more.png'].map(src => {
+    const image = document.createElement('img');
+    image.src = src;
+    return image;
+  });
 
-canvas.addEventListener('click', function(e) {
-  i = i + 1;
-  if (i >= images.length) {
-    i = 0;
-  }
-});
-
-const draw = function() {
-  if (currentX) {
-    if (images[i].complete) {
-      context.drawImage(images[i], currentX - 122, currentY - 35, 243, 70);
+  document.addEventListener('mousemove', function(e) {
+    aimX = e.pageX;
+    aimY = e.pageY;
+    if (currentX === null) {
+      currentX = e.pageX;
+      currentY = e.pageY;
     }
-    currentX = currentX + (aimX - currentX) * 0.1;
-    currentY = currentY + (aimY - currentY) * 0.1;
-  }
+  });
 
-  requestAnimationFrame(draw);
-};
+  canvas.addEventListener('click', function(e) {
+    i = i + 1;
+    if (i >= images.length) {
+      i = 0;
+    }
+  });
 
-const resetButton = document.querySelector('.section--intro .js-reset');
-resetButton.addEventListener('click', function(e) {
-  e.preventDefault();
+  const draw = function() {
+    if (currentX) {
+      if (images[i].complete) {
+        context.drawImage(images[i], currentX - 122, currentY - 35, 243, 70);
+      }
+      currentX = currentX + (aimX - currentX) * 0.1;
+      currentY = currentY + (aimY - currentY) * 0.1;
+    }
 
-  context.clearRect(0, 0, window.innerWidth * 2, window.innerHeight * 2);
-});
-draw();
+    aniFrame = requestAnimationFrame(draw);
+  };
+
+  const resetButton = document.querySelector('.section--intro .js-reset');
+  resetButton.addEventListener('click', function(e) {
+    console.log(context);
+    e.preventDefault();
+    resetCanvas(context);
+  });
+  draw();
+}
+
+function resetCanvas(el) {
+  cancelAnimationFrame(aniFrame);
+  el.clearRect(0, 0, window.innerWidth * 2, window.innerHeight * 2);
+  restartCanvas();
+}
+function restartCanvas() {
+  var countID = setInterval(
+    function(e) {
+      initCanvas();
+      console.log(countID);
+      clearInterval(countID);
+    },
+    3000,
+    this
+  );
+}
+initCanvas();
